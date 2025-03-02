cebbd7129ede78524f15f653ed9a95f544c86687
ZhaoQinlao <59480153+ZhaoQinlao@users.noreply.github.com>
2025-03-02 12:30:10 +0000

    Deploying to gh-pages from @ ZhaoQinlao/ZhaoQinlao.github.io@a08a95c4aad80e4e73466f4ddffa34ab90a3de20 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,7 +16,29 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+        },{id: "post-",
+      
+        title: "",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/2025-03-02-salloc%E9%83%A8%E7%BD%B2deepseek/";
+        
+      },
+    },{id: "post-tas-survey",
+      
+        title: "TAS Survey",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Temporal-Action-Segmentation-An-Analysis-of-Modern-Techniques/";
+        
+      },
+    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
       
         title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
